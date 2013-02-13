6857f94390570dc8cc4ccbdc3334be305771ef94
David Singleton <david@dsingleton.co.uk>
2013-02-13 00:31:34 +0000

    Add underscore to the dependency party, using the requirejs shim to load a non AMD module

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 requirejs.config({
     baseUrl: 'js/components',
+    shim: {
+        'underscore': {
+            exports: '_'
+        }
+    },
     paths: {
-        jquery: 'jquery/jquery.min'
+        jquery: 'jquery/jquery.min',
+        underscore: 'underscore/underscore-min'
     }
 });
 
-// Load jquery, not used by tested for manually in console to test requirejs
-requirejs(['jquery']);
+// Load some dependencies to test them
+requirejs(['jquery', 'underscore'], function($, _){
+    _.each($('html'), function(el) { console.log(el); });
+});
