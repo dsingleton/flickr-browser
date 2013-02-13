2d3360828e3e93499c9c01bb501b52bb597229c6
David Singleton <david@dsingleton.co.uk>
2013-02-13 00:53:37 +0000

    Add Backbone to our dependency list, with a tiny test to make sure it's loaded correctly. Drop the older jquery/underscore test as they're deps of Backbone

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 requirejs.config({
     baseUrl: 'js/components',
     shim: {
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
         'underscore': {
             exports: '_'
         }
     },
     paths: {
         jquery: 'jquery/jquery.min',
-        underscore: 'underscore/underscore-min'
+        underscore: 'underscore/underscore-min',
+        backbone: 'backbone/backbone'
     }
 });
 
-// Load some dependencies to test them
-requirejs(['jquery', 'underscore'], function($, _){
-    _.each($('html'), function(el) { console.log(el); });
+requirejs(['backbone'], function(Backbone){
+    console.log(Backbone);
 });
